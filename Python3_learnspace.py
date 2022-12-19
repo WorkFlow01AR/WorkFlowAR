73d42f408bf7d0af83b0adec32b8b094bcccedb9
WorkFlow01AR <120672294+WorkFlow01AR@users.noreply.github.com>
2022-12-19 15:04:08 -0700

    Spend Sense.py
    An open source app that tracks your spending, and lists, it per account. 
    
    Options should include
    1 The ability to highlight a transaction, so the app will know to make that transaction more visible in the future (ie auto highlight based on item title)
    
    2 Offer you a singular place to handle your finances in a truly private fashion. (Financial Hub)
    
    3 Un-invasive spend tracking (The idea is to not have to give bank details outside of the name of the bank.... Research combining a talkback style screen reader, (That would silently run in the background process, looking for notifications from [insert bank name here]and disregarding everything else.)
    )

---
+++ Python3_learnspace.py
@@ -0,0 +1,19 @@
+{[DiSCLAiMER]; I don't know ANYTHING ABOUT CODING STRUCTURE at all. Syntax, might as well be alian calculous
+That being said, I've created this page because I have a goal, to create my own APP. The purpose of the app\
+is something that is already currently being done, but I feel that I could build an app and have the same functionality\
+without haveing to grant external access to my banking account. Apps like "Trim" that tracks your spending\
+and shows you your recurring charges, and offters to stop the recurring charges. I love the idea of it, right up until
+they say "Enter your full (Routing, Account) numbers. Just can't trust it lol. 
+
+I hear Python is good for app automation... Lets see what I can do.
+} 
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
